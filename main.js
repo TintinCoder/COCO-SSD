7b05ed92961847acf5699b8e580ec4e6ace9bbf9
TintinCoder <Divyamyadav40@gmail.com>
2022-02-14 16:41:44 +0530

    Last Commit

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
 let img = '';
 let status = false;
 let object = [];
+// let randomNum1 = Math.floor(Math.random() * 256);
+// let randomNum2 = Math.floor(Math.random() * 256);
+// let randomNum3 = Math.floor(Math.random() * 256);
+let itemsHtml = document.getElementById('numberOfObjs');
+var video;
+// RGB Values
+// let r = randomNum1;
+// let g = randomNum2;
+// let b = randomNum3;
+
 function preload(){
     img = loadImage('./images/laptop.jpg');
 }
 
 function setup(){
     var canvas;
-    canvas = createCanvas(600, 500)
-    canvas.center();
+    canvas = createCanvas(380, 380)
+    canvas.center();    
 
-    const objectDetector = ml5.objectDetector('cocossd', ()=>{
-        console.log("Model Loaded");
-        status = true;
-        document.getElementById('status').textContent = "Status: Detecting Object";
-        objectDetector.detect(img, gotResults)
-    });
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(380, 380)
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    document.getElementById('status').textContent = "Status: Detecting Object";
 }
 
 function gotResults(error, results){
@@ -30,19 +45,22 @@ function gotResults(error, results){
 }
 
 function draw(){
-    image(img, 0, 0, 600, 500);
+    image(video, 0, 0, 380, 380);
     if(status != "") {
+        r = random(255)
+        g = random(255)
+        b = random(255)
+        objectDetector.detect(video, gotResults);
         for (let i = 0; i < object.length; i++) {
             document.getElementById('status').textContent = "Status: Detected Objects"
-
+            itemsHtml.textContent = `${object.length}`
             let percent = floor(object[i].confidence * 100);
             let objName = object[i].label;
-            fill('64, 175, 227');
+            fill(r, g, b);
             text(`${objName} ${percent}%`, object[i].x + 10, object[i].y + 15);
             noFill()
-            stroke('red')
+            stroke(r, g, b)
             rect(object[i].x, object[i].y, object[i].width, object[i].height);
-
         }
     }
 }
